f959b649e6f9ef3a7a886c3dda6a4bc09191c47b
jo bars <joe.baranski49@gmail.com>
2023-05-27 01:16:30 -0400

    fixed serializer

--- stock/serializers.py
+++ stock/serializers.py
@@ -3,20 +3,22 @@ from django.contrib.auth import get_user_model
 from .models import *
 
 
-
 User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username']
 
+
 class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = '__all__'
 
+
 class CurrentStockSerializer(serializers.ModelSerializer):
     class Meta:
         model = currentStock
-        field = '__all__'
+        fields = '__all__'
