8b1b2d708a18b9e8568dec9f8c3da024da6ba237
Amber Jankowski <97934285+AmJann@users.noreply.github.com>
2023-05-27 15:47:44 -0400

    deploy

--- stock_django/settings.py
+++ stock_django/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'click_django.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-'default': dj_database_url.config(default= conn_max_age=600)
+'default': dj_database_url.config(conn_max_age=600    )
 }
 
 
