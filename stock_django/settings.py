0950e0d3004e5ab8f2eea0bc590005971d6754d8
Amber Jankowski <97934285+AmJann@users.noreply.github.com>
2023-05-28 09:42:06 -0400

    updated database var

--- stock_django/settings.py
+++ stock_django/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'stock_django.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-'default': dj_database_url.config(conn_max_age=600    )
+'default': dj_database_url.config(default ='postgres://click_user:oNWBLX32WSuC6hDt4keqX2KeGuVtbVWv@dpg-cgt2vj02qv2fdeeq1gc0-a/click_comp'conn_max_age=600    )
 }
 
 
